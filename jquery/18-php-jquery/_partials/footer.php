62914e57dcf26c115dd1b60ce2ec009d9691a820
reisthi <thiagoth.reis@gmail.com>
2019-09-12 11:07:35 -0500

    some docs

--- jquery/18-php-jquery/_partials/footer.php
+++ jquery/18-php-jquery/_partials/footer.php
@@ -1,3 +1,12 @@
 </div>
+
+<br><br><br><br>
+<hr>
+<p>For this class I used:</p>
+<ul>
+    <li>A php server: <a href='https://www.php.net/manual/en/features.commandline.webserver.php'>More info</a></li>
+    <li>A msql db named sakila: <a href='https://dev.mysql.com/doc/index-other.html'>More info</a></li>
+    <li>A MAMP server: <a href='https://www.mamp.info/en/'>More info</a></li>
+</ul>
 </body>
 </html>
